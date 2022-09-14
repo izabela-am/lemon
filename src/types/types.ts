c7d84ba8c671f1445ac4e5fa73f1bc63daec4393
Izabela <izabela.m.alfenas@gmail.com>
2022-09-14 17:18:05 -0300

    ft: define types

---
+++ src/types/types.ts
@@ -0,0 +1,9 @@
+type ConnectionTypes = 'monofasico' | 'bifasico' | 'trifasico';
+type ConsumptionClasses = 'residencial' | 'industrial' | 'comercial' | 'rural' | 'poderPublico';
+type TaxModality = 'azul' | 'branca' | 'verde' | 'convencional';
+
+export {
+  ConsumptionClasses,
+  TaxModality,
+  ConnectionTypes,
+};
